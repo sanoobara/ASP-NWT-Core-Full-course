5972227105e09582896eca86117f2932717569e5
Tom Brown <tombrown@live.com>
2025-01-02 23:44:29 +0300

    first commit

---
+++ GameStore/Program.cs
@@ -0,0 +1,15 @@
+using GameStore.Endpoints;
+using GameStore.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
+
+
+var app = builder.Build();
+
+app.MapGamesEndpoints();
+
+app.MigrateDb();
+app.Run();
